7b8d06211ab653d22b24e647c25b6c48951643e6
Ziggy Shea <zigs_17@hotmail.com>
2019-01-27 17:19:04 -0800

    Added testing for settings page

--- client/cypress/integration/app.spec.js
+++ client/cypress/integration/app.spec.js
@@ -12,6 +12,9 @@ describe('Auth redirects', () => {
 });
 
 describe('Logging in user', () => {
+    beforeEach(() => {
+        Cypress.Cookies.preserveOnce('sessionId', 's%3AFO6WA5QqysEZlbgCXHHiBGwFKh3XhQvg.mUWql6NVQSyd9u7nBCQ%2FcmiAG2bEN2KgsBIZ84hkex0')
+    });
     it('renders signin page with errors if user info incorrect', () => {
         cy.get('input[name=handle]').type('yeezus');
         cy.get('input[name=password]').type('wrong');
@@ -30,5 +33,13 @@ describe('Logging in user', () => {
         cy.get('.profileNav').contains('yeezus\'s Friends').click();
         cy.get('.FriendsList').should('have.descendants', 'a');
         cy.get('.Comments').should('have.descendants', '.Comment');
+    });
+    it('should load settings correctly', () => {
+        cy.get('.navSection').contains('Settings').click();
+        cy.get('input[name=name]').should('have.value', 'Kanye');
+        cy.get('a').contains('Update Seen').click();
+        cy.get('.updateSeen').should('exist');
+        cy.get('a').contains('Venues').click();
+        cy.get('.input-group-text').should('contain', 'Rogers Arena');
     });
 });
